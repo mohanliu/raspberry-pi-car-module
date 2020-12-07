070d27d71d31fe47be3a5a5d947e16b8e796100a
mohanliu <mohan@u.northwestern.edu>
2020-12-06 21:59:33 -0600

    Add two direction function; add input range

--- Code/Server/servo.py
+++ Code/Server/servo.py
@@ -1,5 +1,6 @@
 import time
 from PCA9685 import PCA9685
+import argparse
 
 class Servo:
     def __init__(self):
@@ -27,17 +28,39 @@ class Servo:
         elif channel == '7':
             self.PwmServo.setServoPulse(15, 500+int((angle+error)/0.09))
 
-# Main program logic follows:
-if __name__ == '__main__':
+def initial_position():
     pwm = Servo()
     pwm.setServoPwm('0', 90)
     pwm.setServoPwm('1', 90)
 
-    
+def head_nod(step):
+    step = max(step, 60)
+    step = min(step, 180)
 
-    
-       
+    pwm = Servo()
+    pwm.setServoPwm('1', step)
+
+def head_shake(step):
+    step = max(step, 5)
+    step = min(step, 175)
+
+    pwm = Servo()
+    pwm.setServoPwm('0', step)
 
 
+# Main program logic follows:
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--initialization", "-I", action="store_true", help="Initialize head")
+    parser.add_argument("--nod", "-N", type=int, help="Nodding degree")
+    parser.add_argument("--shake", "-S", type=int, help="shaking degree")
+    args = parser.parse_args()
 
-    
+    if args.initialization:
+        initial_position()
+
+    if args.nod:
+        head_nod(args.nod)
+
+    if args.shake:
+        head_shake(args.shake)
