55c52af85c0420b481acb7643c5847146b2c0abb
mohanliu <mohan@u.northwestern.edu>
2020-12-08 00:13:51 -0600

    clean code

--- Code/Server/server.py
+++ Code/Server/server.py
@@ -23,37 +23,42 @@ from Command import COMMAND as cmd
 
 class Server:   
     def __init__(self):
-        self.PWM=Motor()
-        self.servo=Servo()
-        self.led=Led()
-        self.ultrasonic=Ultrasonic()
-        self.buzzer=Buzzer()
-        self.adc=Adc()
-        self.light=Light()
-        self.infrared=Line_Tracking()
+        self.PWM = Motor()
+        self.servo = Servo()
+        self.led = Led()
+        self.ultrasonic = Ultrasonic()
+        self.buzzer = Buzzer()
+        self.adc = Adc()
+        self.light = Light()
+        self.infrared = Line_Tracking()
         self.tcp_Flag = True
-        self.sonic=False
-        self.Light=False
+        self.sonic = False
+        self.Light = False
         self.Mode = 'one'
-        self.endChar='\n'
-        self.intervalChar='#'
+        self.endChar ='\n'
+        self.intervalChar = '#'
+        
     def get_interface_ip(self):
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        return socket.inet_ntoa(fcntl.ioctl(s.fileno(),
-                                            0x8915,
-                                            struct.pack('256s',b'wlan0'[:15])
-                                            )[20:24])
+        return socket.inet_ntoa(
+            fcntl.ioctl(
+                s.fileno(),
+                0x8915,
+                struct.pack('256s',b'wlan0'[:15])
+            )[20:24]
+        )
+    
     def StartTcpServer(self):
-        HOST=str(self.get_interface_ip())
+        HOST = str(self.get_interface_ip())
         self.server_socket1 = socket.socket()
-        self.server_socket1.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEPORT,1)
+        self.server_socket1.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEPORT, 1)
         self.server_socket1.bind((HOST, 5000))
         self.server_socket1.listen(1)
         self.server_socket = socket.socket()
-        self.server_socket.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEPORT,1)
+        self.server_socket.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEPORT, 1)
         self.server_socket.bind((HOST, 8000))              
         self.server_socket.listen(1)
-        print('Server address: '+HOST)
+        print('Server address: {}'.format(HOST))
         
         
     def StopTcpServer(self):
@@ -70,8 +75,10 @@ class Server:
         self.ReadData=Thread(target=self.readdata)
         self.SendVideo.start()
         self.ReadData.start()
-    def send(self,data):
+        
+    def send(self, data):
         self.connection1.send(data.encode('utf-8'))    
+        
     def sendvideo(self):
         try:
             self.connection,self.client_address = self.server_socket.accept()
@@ -260,7 +267,8 @@ class Server:
                             pass
         except Exception as e: 
             print(e)
-        self.StopTcpServer()    
+        self.StopTcpServer()  
+        
     def sendUltrasonic(self):
         if self.sonic==True:
             ADC_Ultrasonic=self.ultrasonic.get_distance()
@@ -271,16 +279,18 @@ class Server:
                     self.sonic=False
             self.ultrasonicTimer = threading.Timer(0.13,self.sendUltrasonic)
             self.ultrasonicTimer.start()
+            
     def sendLight(self):
-        if self.Light==True:
-            ADC_Light1=self.adc.recvADC(0)
-            ADC_Light2=self.adc.recvADC(1) 
+        if self.Light == True:
+            ADC_Light1 = self.adc.recvADC(0)
+            ADC_Light2 = self.adc.recvADC(1) 
             try:
                 self.send(cmd.CMD_LIGHT+'#'+str(ADC_Light1)+'#'+str(ADC_Light2)+'\n')
             except:
-                self.Light=False
-            self.lightTimer = threading.Timer(0.17,self.sendLight)
+                self.Light = False
+            self.lightTimer = threading.Timer(0.17, self.sendLight)
             self.lightTimer.start()
+            
     def Power(self):
         while True:
             ADC_Power=self.adc.recvADC(2)*3
@@ -299,5 +309,6 @@ class Server:
                     time.sleep(0.1)
             else:
                 self.buzzer.run('0')
+                
 if __name__=='__main__':
     pass
